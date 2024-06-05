397712d929bd12c24e215d60793abe6048c97d84
Phạm Duy Tuấn <130920493+Gokycha@users.noreply.github.com>
2024-06-05 12:09:07 +0700

    Update telegramBot.py

--- telegramBot.py
+++ telegramBot.py
@@ -17,18 +17,18 @@ logger = logging.getLogger(__name__)
 # Hàm start khi người dùng bắt đầu trò chuyện với bot
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Thứ này được tạo ra để nhắc nhở ae đi ăn, lên ngủ trưa và về đúng giờ')
-    CHAT_ID = update.message.chat_id
-    print(CHAT_ID)
     while True:
         # if CHAT_ID:
         #     print(CHAT_ID)
         #     await app.bot.send_message(chat_id=CHAT_ID, text='test')
         if datetime.datetime.now().strftime("%I:%M:%S %p") == "11:30:00 AM":
-            await app.bot.send_message(chat_id=CHAT_ID, text='Tất cả đi ăn trưa!!!')
-        await asyncio.sleep(10)  
+            await update.message.reply_text('Tất cả đi ăn trưa!!!')
+        await asyncio.sleep(10) 
+        
+async def test(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text('test is ok')
             
 def error(update, context):
-    """Log các lỗi"""
     logger.warning('Có lỗi: %s', context.error)
         
 app =  Application.builder().token(TOKEN).build()
